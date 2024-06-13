773632ed7daa8cbafa6ab7b48ab0c5c31a1933dc
Bhirawa Mbani <bhirmbani@gmail.com>
2024-06-13 17:28:55 +0700

    init vue view

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
+import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [EventsModule],
+  controllers: [AppController],
+  providers: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'views', 'dist'),
+    }),
+    EventsModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
 
   await app.listen(3000);
+  app.setBaseViewsDir(join(__dirname, '..', '..', 'views', 'dist'));
+  app.useStaticAssets(join(__dirname, '..', '..', 'views', 'dist'));
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  app.engine('html', require('ejs').renderFile);
+  app.setViewEngine('html');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
