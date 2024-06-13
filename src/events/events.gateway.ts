5cefefd9fd13b0931775496f2b8e8160a3bfcbd3
Bhirawa Mbani <bhirmbani@gmail.com>
2024-06-13 18:22:58 +0700

    update client

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -5,8 +5,7 @@ import {
   WebSocketServer,
   WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Server } from 'ws';
 
 @WebSocketGateway(8080)
@@ -18,8 +17,19 @@ export class EventsGateway {
   onEvent(client: any, data: any): Observable<WsResponse<{ message: string }>> {
     console.log('dari client ->', data);
     const serverMessage = 'Message from server';
-    return from([{ message: serverMessage }]).pipe(
-      map((item) => ({ event: 'events', data: item })),
+    let counter = 0;
+    return new Observable<WsResponse<{ message: string; second: number }>>(
+      (observer) => {
+        const intervalId = setInterval(() => {
+          counter++;
+          observer.next({
+            event: 'events',
+            data: { message: serverMessage, second: counter },
+          });
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+      },
     );
   }
 
